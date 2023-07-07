19079cfdfe681a0426797afaf71ad6b5e4f84ae9
Dumitru Hristov <dima.hristov@gmail.com>
2023-07-07 07:03:57 +0300

    modified grid item; added placeholder;

--- app/src/main/java/com/cannonades/petconnect/animalsnearyou/presentation/HomeScreen.kt
+++ app/src/main/java/com/cannonades/petconnect/animalsnearyou/presentation/HomeScreen.kt
@@ -1,24 +1,24 @@
 package com.cannonades.petconnect.animalsnearyou.presentation
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
+import com.cannonades.petconnect.R
 import com.cannonades.petconnect.common.presentation.model.UIAnimal
 
 
@@ -45,7 +45,7 @@ fun HomeScreen(
 ) {
     when {
         viewState.failure != null -> {
-            /*the 'no network' failure is taken care of in MainActivity. TODO: process other failures here*/
+            /*TODO: process no more animals here or show a Toast (then remove this conditional branch)*/
             AnimalGrid(animals = viewState.animals, viewState.loading, onEvent = onEvent)
         }
 
@@ -86,18 +86,12 @@ fun AnimalGrid(animals: List<UIAnimal>, isLoading: Boolean, onEvent: (HomeEvent)
 
 @Composable
 fun UIAnimalComposable(animal: UIAnimal) {
-    Column(modifier = Modifier.padding(16.dp)) {
-        val painter = rememberAsyncImagePainter(model = animal.name)
-
-        Card(
-            elevation = CardDefaults.elevatedCardElevation(),
-            shape = RoundedCornerShape(15.dp),
-        ) {
-            Image(
-                painter = painter,
-                contentDescription = "Animal image",
-                modifier = Modifier.size(200.dp)
-            )
-        }
+    Box(Modifier.padding(4.dp)) {
+        AsyncImage(
+            model = animal.name,
+            contentDescription = "Animal image",
+            placeholder = painterResource(R.drawable.ic_jetnews_logo),
+            modifier = Modifier.size(200.dp)
+        )
     }
 }
