511f2ccee9a597f623903cd5736331eb0d08728d
Dumitru Hristov <dima.hristov@gmail.com>
2023-07-22 19:46:42 +0300

    fixed calling io ops in UI thread;

--- app/src/main/java/com/cannonades/petconnect/animalsnearyou/domain/usescases/GetAnimalsNoCategoryFromCacheUseCase.kt
+++ app/src/main/java/com/cannonades/petconnect/animalsnearyou/domain/usescases/GetAnimalsNoCategoryFromCacheUseCase.kt
@@ -1,8 +1,17 @@
 package com.cannonades.petconnect.animalsnearyou.domain.usescases
 
+import com.cannonades.petconnect.common.domain.model.Animal
 import com.cannonades.petconnect.common.domain.repositories.AnimalRepository
+import com.cannonades.petconnect.common.utils.DispatchersProvider
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
-class GetAnimalsNoCategoryFromCacheUseCase @Inject constructor(private val animalRepository: AnimalRepository) {
-    operator fun invoke() = animalRepository.getAnimalsNoCategoryFromDb()
+class GetAnimalsNoCategoryFromCacheUseCase @Inject constructor(
+    private val animalRepository: AnimalRepository,
+    private val dispatchersProvider: DispatchersProvider
+) {
+    operator fun invoke(): Flow<List<Animal>> =
+        animalRepository.getAnimalsNoCategoryFromDb()
+            .flowOn(dispatchersProvider.io())
 }
--- app/src/main/java/com/cannonades/petconnect/categories/domain/usecases/GetCategoriesFromCacheUseCase.kt
+++ app/src/main/java/com/cannonades/petconnect/categories/domain/usecases/GetCategoriesFromCacheUseCase.kt
@@ -1,8 +1,17 @@
 package com.cannonades.petconnect.categories.domain.usecases
 
+import com.cannonades.petconnect.common.domain.model.Category
 import com.cannonades.petconnect.common.domain.repositories.CategoriesRepository
+import com.cannonades.petconnect.common.utils.DispatchersProvider
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
-class GetCategoriesFromCacheUseCase @Inject constructor(private val categoriesRepository: CategoriesRepository) {
-    operator fun invoke() = categoriesRepository.getCategoriesFromDb()
+class GetCategoriesFromCacheUseCase @Inject constructor(
+    private val categoriesRepository: CategoriesRepository,
+    private val dispatchersProvider: DispatchersProvider
+) {
+    operator fun invoke(): Flow<List<Category>> =
+        categoriesRepository.getCategoriesFromDb()
+            .flowOn(dispatchersProvider.io())
 }
