56d9f966e23a4678c59084475d2d7fb50e02e34e
Dumitru Hristov <dima.hristov@gmail.com>
2023-07-23 04:11:29 +0300

    show cat image in the AnimalScreen;

--- app/src/main/java/com/cannonades/petconnect/common/presentation/ui/AnimalScreen.kt
+++ app/src/main/java/com/cannonades/petconnect/common/presentation/ui/AnimalScreen.kt
@@ -1,27 +1,36 @@
 package com.cannonades.petconnect.common.presentation.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.layout.ContentScale
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.AsyncImage
 
 @Composable
 fun AnimalScreen(animalId: String, viewModel: AnimalViewModel = hiltViewModel()) {
-    LaunchedEffect(key1 = animalId) {
+    LaunchedEffect(animalId) {
         viewModel.loadAnimal(animalId)
     }
 
     val animalData by viewModel.animal.collectAsState()
 
-    Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "ID: ${animalData?.id ?: "Loading..."}")
-        Text(text = "photo object: ${animalData?.photo ?: "Loading..."}")
-        Text(text = "Photo URL: ${animalData?.photo?.url ?: "Loading..."}")
+    val imageWidth = animalData?.photo?.width?.toFloat() ?: 1f
+    val imageHeight = animalData?.photo?.height?.toFloat() ?: 1f
+
+    animalData?.photo?.url?.let { url ->
+        AsyncImage(
+            model = url,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(imageWidth / imageHeight),
+            contentScale = ContentScale.Crop
+        )
     }
+    
 }
