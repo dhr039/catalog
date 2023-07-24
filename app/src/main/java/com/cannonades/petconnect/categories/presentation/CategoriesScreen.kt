5abbd097bf6ead0cb96f2b4dd345b5a73848a969
Dumitru Hristov <dima.hristov@gmail.com>
2023-07-24 15:22:24 +0300

    better looking categories screen;

--- app/src/main/java/com/cannonades/petconnect/categories/presentation/CategoriesScreen.kt
+++ app/src/main/java/com/cannonades/petconnect/categories/presentation/CategoriesScreen.kt
@@ -1,14 +1,23 @@
 package com.cannonades.petconnect.categories.presentation
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -56,14 +65,45 @@ fun CategoriesList(
     categories: List<UICategory>,
     openCategoryScreen: (String) -> Unit
 ) {
-    Column {
-        categories.forEach { category ->
-            Card(
-                modifier = modifier
-                    .padding(20.dp)
-                    .clickable { openCategoryScreen(category.id) }) {
-                Text(category.name)
+    LazyColumn(
+        modifier = modifier.padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(categories.size) { index ->
+            val category = categories[index]
+            CategoryCard(
+                category = category,
+                onClick = { openCategoryScreen(category.id) }
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CategoryCard(category: UICategory, onClick: () -> Unit) {
+    Card(
+        shape = MaterialTheme.shapes.small,
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(vertical = 12.dp, horizontal = 16.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column {
+                Text(text = category.name, style = MaterialTheme.typography.bodyLarge)
             }
+
+            Icon(
+                imageVector = Icons.Default.ArrowForward,
+                contentDescription = null
+            )
         }
     }
 }
