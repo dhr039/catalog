94ce6a12996b87e268363f87f6f2c463080d4f9f
Dumitru Hristov <dima.hristov@gmail.com>
2023-08-12 12:22:27 +0300

    higher Compose and other libs versions;

--- app/src/androidTest/java/com/cannonades/petconnect/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/cannonades/petconnect/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.cannonades.petconnect", appContext.packageName)
+        assertEquals("com.drollgames.catsii", appContext.packageName)
     }
 }
